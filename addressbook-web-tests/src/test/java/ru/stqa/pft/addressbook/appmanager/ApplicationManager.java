9185b0a8f269a2b90cdbf1a0786aa2ae083de331
vkonovki <Valeriia.Konovkina@t-systems.com>
2021-03-17 00:16:57 +0300

    3 browsers task

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -2,7 +2,11 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -13,10 +17,24 @@ public class ApplicationManager {
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
   private ContactHelper contactHelper;
- ;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
+
+  ;
 
   public void init() {
-    wd = new FirefoxDriver();
+
+    if (browser.equals(BrowserType.FIREFOX)){
+      wd = new FirefoxDriver();
+    } else if (browser.equals(BrowserType.CHROME)){
+      wd = new ChromeDriver();
+    } else if (browser.equals(BrowserType.IE)){
+      wd = new EdgeDriver();
+    }
+
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost:8090/addressbook/");
     groupHelper = new GroupHelper(wd);
